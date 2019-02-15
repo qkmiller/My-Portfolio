4bb111bf8493b350327262ce825dd7716efb25bb
Quinn Miller <millerquinn66@gmail.com>
2019-02-15 12:35:07 -0800

    add paralax placeholders and title to home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Parallax} from 'react-materialize';
 
+const textCenter = {
+  textAlign: 'center'
+}
 
 function Home(){
   return (
     <div>
-      Home
+      <Parallax imageSrc="http://materializecss.com/images/parallax1.jpg"/>
+      <div className="section white">
+        <div className="row container" style={textCenter}>
+          <h2 className="header">Quinn Miller</h2>
+          <p className="grey-text text-darken-3 lighten-3" >Junior web developer, specializing in backend logic.</p>
+        </div>
+      </div>
+      <Parallax imageSrc="http://materializecss.com/images/parallax2.jpg"/>
     </div>
   );
 }
