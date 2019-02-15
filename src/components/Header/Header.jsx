d7f47202cc091d58eda5e65cd4767feca515abb6
Quinn Miller <millerquinn66@gmail.com>
2019-02-15 14:42:59 -0800

    insert links to AboutMe and MyWork routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Navbar, NavItem} from 'react-materialize';
+import {Link} from 'react-router-dom';
 
 const brandText = <span className="black-text" style={{marginLeft: '50px'}}>Quinn Miller</span>;
-const aboutMeLink = <span className="black-text">About Me</span>;
-const myWorkLink = <span className="black-text">My Work</span>;
-
+const aboutMeLink = <Link className="black-text" to="/about-me">About Me</Link>;
+const myWorkLink = <Link className="black-text" to="/my-work">My Work</Link>;
 
 function Header(){
   return (
     <div>
       <Navbar className='black-text white' brand={brandText} right>
-        <NavItem onClick={() => console.log('test click')}>{aboutMeLink}</NavItem>
-        <NavItem onClick={() => console.log('test click')}>{myWorkLink}</NavItem>
+        <NavItem>{aboutMeLink}</NavItem>
+        <NavItem>{myWorkLink}</NavItem>
       </Navbar>
     </div>
   );
