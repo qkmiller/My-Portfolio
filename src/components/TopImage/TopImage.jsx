daa61478d37f71aee779243829252d36570bcbe7
Quinn Miller <millerquinn66@gmail.com>
2019-02-15 13:51:21 -0800

    move parallax image to its own component

---
+++ src/components/TopImage/TopImage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Parallax} from 'react-materialize';
+
+function TopImage(){
+  return (
+    <div>
+      <Parallax imageSrc="http://materializecss.com/images/parallax1.jpg"/>
+    </div>
+  );
+}
+
+export default TopImage;
