87ac9e4091ec2228582597e91f5c304d60feb03e
Quinn Miller <millerquinn66@gmail.com>
2019-02-15 13:55:23 -0800

    import TopImage component into app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './Home/Home';
 import Header from './Header/Header';
+import TopImage from './TopImage/TopImage';
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App(){
   return (
     <div>
       <Header />
+      <TopImage />
       <Switch>
         <Route exact path='/' component={Home} />
       </Switch>
