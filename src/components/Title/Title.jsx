1ae4f59fddb10cd7df5b118c3249a30fb7531907
Quinn Miller <millerquinn66@gmail.com>
2019-02-15 14:32:12 -0800

    make my title a re-useable component

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {textCenter} from '../../Styles'
+
+function Title(){
+  return (
+    <div className="section white">
+      <div className="row container" style={textCenter}>
+        <h2 className="header">Quinn Miller</h2>
+        <p className="grey-text text-darken-3 lighten-3" >Junior Web Developer | Audio Engineer | Student</p>
+      </div>
+    </div>
+  );
+}
+
+export default Title;
