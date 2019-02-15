db9900211b77026aa04d7e12cec487203cef6fdd
Quinn Miller <millerquinn66@gmail.com>
2019-02-15 14:33:53 -0800

    import and use Title component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-
-const textCenter = {
-  textAlign: 'center'
-};
+import Title from '../Title/Title';
 
 function Home(){
   return (
     <div>
       <div className="section white">
-        <div className="row container" style={textCenter}>
-          <h2 className="header">Quinn Miller</h2>
-          <p className="grey-text text-darken-3 lighten-3" >Junior web developer, specializing in backend logic.</p>
+        <div className="row container">
+          <Title />
         </div>
       </div>
     </div>
