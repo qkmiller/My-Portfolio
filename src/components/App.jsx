b720d6c30c83d234d5fef8f5736a4f6adbbacd67
Quinn Miller <millerquinn66@gmail.com>
2019-02-15 11:24:46 -0800

    import header into app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './Home/Home';
+import Header from './Header/Header';
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -7,6 +8,7 @@ import { Switch, Route } from 'react-router-dom';
 function App(){
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Home} />
       </Switch>
