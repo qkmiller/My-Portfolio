0629db4a6ec37ee73facd82c0c88d58ef457495d
Quinn Miller <millerquinn66@gmail.com>
2019-02-15 14:39:46 -0800

    add MyWork component

---
+++ src/components/MyWork/MyWork.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyWork(){
+  return (
+    <div class="row container">
+      <h2>Here is some of my work as a programmer</h2>
+    </div>
+  );
+}
+
+export default MyWork;
