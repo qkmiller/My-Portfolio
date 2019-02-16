2665afaf6cfbc367ff5f7b747cbfd34aec1b5ca0
Quinn Miller <millerquinn66@gmail.com>
2019-02-15 18:13:54 -0800

    edit the contents of AboutMe differentiate it from my cover letter

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import Title from '../Title/Title';
 
 function AboutMe(){
   return (
-    <div class="row container">
-      <h2>Quinn Miller</h2>
-      <h6>Junior Web Developer | Audio Engineer</h6>
+    <div className="row container">
+      <Title />
       <hr />
-      <p>Hi! Thanks taking the time to read my coverletter. I am currently a student at Epicodus, a coding bootcamp in Portland Oregon. My education at Epicodus has given me many of the technical skills I need to be a great team-member of CD Baby. I decided to attend Epicodus because of my passion for the web and interest in open-source development. I started by familiarizing myself with the command line and Git, while making web-pages using html and css. Later in the course i learned Javascript and Ruby along with the basics of SQL. I have no doubt about my ability to learn new things on the job, given my familiarity with similar technologies and my track record of learning as I go.</p>
+      <p><span style={{marginLeft: '10px'}}>Thanks </span> for taking the time visit my portfolio. I am currently a student at Epicodus, a coding bootcamp in Portland Oregon. My education at Epicodus has given me great core programming fundamentals, and experience with cutting edge frameworks. I decided to attend Epicodus because of my passion for technology and interest in open-source development. I started by familiarizing myself with the command line and Git, while making web-pages using html and css. Later i learned Javascript, then Ruby along with Rails and postgreSQL. I have no doubt about my ability to learn any new technology the programming world has to offer.</p>
       <hr />
-      <p>Before Epicodus, I worked as a customer service associate for QFC. I strove for excellence and customer satisfaction in my work as a cashier and produce clerk, making sure i resolve issues thoroughly and professionally. Also informing the customer of the product they are buying and pointing out any problems or caviates. The many years i spent doing this job shows that i work well with a diverse, fluidly changing team, in paralell with the general public. My deliverables showed that my detail-oriented approach paid off, as I was consistently in the top 10% of representatives for customer satisfaction.</p>
+      <p>Before Web development, I worked as a customer service associate for QFC. I strove for excellence and customer satisfaction in my work as a cashier, making sure i resolve issues thoroughly and professionally. Also informing the customer of the product they are buying and pointing out any problems or caviates. While this experience may not be relevent to my programming skills, it does, however, attest to my core values as a human-being. The many years i spent doing this job shows that i work well with a diverse, fluid, constantly changing team.</p>
       <hr />
-      <p>Music has always been a part of my life. During my highschool years, learning to play my favorite songs on guitar became an obsession. I played in a few bands, playing house shows and venues here and there. After learning to record music on my computer, my passion moved to producing and engineering my own music. This has led me to focus solely on independant artists and labels.</p>
+      <p>Music has always been a part of my life. During my highschool years, learning to play my favorite songs on guitar became an obsession. I played in a few bands, playing house shows and venues here and there. After learning to record music on my computer, my passion moved to producing and engineering my own music. This eventually turned into a passion for audio engineering and sound design. The most important parts of this on-going endeavor are the details. Sometimes the smallest changes can make the biggest difference.</p>
       <hr />
-      <p>I think this would be a great opportunity to apply my tech skills to something that I'm passionate about. Thanks!</p>
+      <p>If you would like to know more, please contact me through any of <a>these</a> methods</p>
     </div>
   );
 }
