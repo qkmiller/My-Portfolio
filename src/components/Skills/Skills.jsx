a312aa868ac83dc1c7afe7f784f6cdcdb2bb145b
Quinn Miller <millerquinn66@gmail.com>
2019-02-22 12:24:20 -0800

    Add collapsible for each skill

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import {Row, Col, CardPanel} from 'react-materialize';
+import {Collapsible, CollapsibleItem} from 'react-materialize';
+import PropTypes from 'prop-types';
 
-function Skills(){
+function Skills(props){
+
   return (
     <div>
       <h4>Skills</h4>
-      <Row>
-        <Col s={12} m={5}>
-          <CardPanel className="teal lighten-4 black-text">
-            <span>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.</span>
-          </CardPanel>
-        </Col>
-        <Col s={12} m={7}>
-          For a simpler card with less markup, try using a card panel which just has padding and a shadow effect
-        </Col>
-      </Row>
+      <Collapsible accordion defaultActiveKey={1}>
+        {}
+        <CollapsibleItem header={props.skills.javascript.name} icon='filter_drama'>
+          {props.skills.javascript.description}
+        </CollapsibleItem>
+        <CollapsibleItem header={props.skills.ruby.name} icon='place'>
+          {props.skills.ruby.description}
+        </CollapsibleItem>
+        <CollapsibleItem header={props.skills.sql.name} icon='whatshot'>
+          {props.skills.sql.description}
+        </CollapsibleItem>
+      </Collapsible>
     </div>
   );
 }
+
+Skills.PropTypes = {
+  skills: PropTypes.object
+};
 
 export default Skills;
