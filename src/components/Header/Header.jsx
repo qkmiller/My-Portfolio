a7788e41d9d9940012f5c6caf49e3acf2c993f22
Quinn Miller <millerquinn66@gmail.com>
2019-02-24 22:12:38 -0800

    import and style a picture of me for the navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import {Navbar, NavItem} from 'react-materialize';
 import {styleNavItemText, styleHeaderTitle} from '../../Styles';
+import quinn from '../../assets/images/quinn.png';
 
-
+function icon(image) {
+  return <img style={{
+    width: '80px', 
+    borderRadius:'30px', 
+    borderWidth: '2px', 
+    borderStyle: 'solid', 
+    boxShadow: '0px 2px 5px 0 rgba(0,0,0,0.20), 0px 4px 10px 0 rgba(0,0,0,0.20), 0px 3px 8px -2px rgba(0,0,0,0.20)', 
+    margin: '10px 10px 10px 10px'}}
+    src={image}/>
+}
 
 function Header(){
 
   return (
     <div>
-      <Navbar fixed href='/#/' className='black-text white' brand={styleHeaderTitle('Quinn Miller')} right>
+      <Navbar fixed href='/#/' className='black-text white' brand={icon(quinn)} right>
         <NavItem href="#aboutme">{styleNavItemText('About Me')}</NavItem>
         <NavItem href="#mywork">{styleNavItemText('My Work')}</NavItem>
       </Navbar>
