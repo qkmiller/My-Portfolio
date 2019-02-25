772612df9d50e80be2242e6f9fa64bf95e1a54d7
Quinn Miller <millerquinn66@gmail.com>
2019-02-24 19:32:05 -0800

    Add collapsible for each project in MyWorkList.json to MyWork component

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {Collapsible, CollapsibleItem} from 'react-materialize';
 
-function MyWork(){
+function MyWork(props){
   return (
     <div class="row container">
-      <h2>Here is some of my work as a programmer</h2>
+      <h5>Here is some of my work as a programmer</h5>
+      <Collapsible accordion>
+        {props.projects.map((project, index) => 
+          <CollapsibleItem key={index} header={project.name}>
+            {project.description}
+          </CollapsibleItem>
+        )}
+      </Collapsible>
     </div>
   );
 }
-
+MyWork.propTypes = {
+  projects: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+  })).isRequired
+};
 export default MyWork;
