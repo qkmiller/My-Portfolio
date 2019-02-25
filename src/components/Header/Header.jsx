5eb1d28316f170a10b9745025a2ab955155bfa60
Quinn Miller <millerquinn66@gmail.com>
2019-02-24 20:02:20 -0800

    Make navbar links scroll to diffent locations instead of routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,9 +8,9 @@ function Header(){
 
   return (
     <div>
-      <Navbar href='/#/' className='black-text white' brand={styleHeaderTitle('Quinn Miller')} right>
-        <NavItem href="/#/about-me">{styleNavItemText('About Me')}</NavItem>
-        <NavItem href="/#/my-work">{styleNavItemText('My Work')}</NavItem>
+      <Navbar fixed href='/#/' className='black-text white' brand={styleHeaderTitle('Quinn Miller')} right>
+        <NavItem href="#aboutme">{styleNavItemText('About Me')}</NavItem>
+        <NavItem href="#mywork">{styleNavItemText('My Work')}</NavItem>
       </Navbar>
     </div>
   );
