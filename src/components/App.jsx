0af4dd8814c99ae8a3796b7056be4705de087ead
Quinn Miller <millerquinn66@gmail.com>
2019-02-24 19:21:30 -0800

    Remove routes for MyWork and AboutMe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import Home from './Home/Home';
 import Header from './Header/Header';
 import TopImage from './TopImage/TopImage';
-import AboutMe from './AboutMe/AboutMe';
-import MyWork from './MyWork/MyWork';
-
-import { Switch, Route } from 'react-router-dom';
 
 
 function App(){
@@ -13,14 +9,8 @@ function App(){
     <div>
       <Header />
       <TopImage />
-      <div className='section white'>
-        <div className='row container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about-me' component={AboutMe} />
-            <Route exact path='/my-work' component={MyWork} />
-          </Switch>
-        </div>
+      <div className='row container'>
+        <Home />
       </div>
     </div>
   );
